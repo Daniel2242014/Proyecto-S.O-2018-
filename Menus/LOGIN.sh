2ff12edbc91c3ebbc4bbd5e4a6bf52a72fffb101
agusasdfkajsd <agus-102@hotmail.com>
2018-10-02 02:12:05 -0400

    Cambio

--- Menus/LOGIN.sh
+++ Menus/LOGIN.sh
@@ -1,39 +1,48 @@
 #!/bin/bash
-usuario=1
-while test $usuario -ne -1 #Agusitn te puse un while para una prueba si queres eliminalo
-do
 clear
+tput setab 0
 tput setaf 4
-echo -e "                                                   \e[0;90m____________"
-echo -e "                                                  \e[0;90m|\e[0;35mXXXXXXXXXXXX\e[0;90m|"
-echo -e "                                                  \e[0;90m|\e[0;35mX    XX    x\e[0;90m|"
-echo -e "                                                  \e[0;90m|\e[0;35mX    XX    x\e[0;90m|"
-echo -e "                                           \e[0;90m_______\e[0;90m|\e[0;35mX\e[0;90m__\e[0;35mXXXXXXXXX\e[0;90m|"
-echo -e "                                          \e[0;90m|\e[0;31mXXXXXXXXXXX\e[0;90m| \e[0;35mXX    X\e[0;90m|"
-echo -e "                                          \e[0;90m|\e[0;31mX   XXX   X\e[0;90m| \e[0;35mXX    X\e[0;90m|"
-echo -e "\e[0;34;1m _____                                    \e[0;90m|\e[0;31mX   XXX   X\e[0;90m|\e[0;35mXXXXXXXX\e[0;90m|"
-echo -e "\e[0;34;1m|  ___|   _ _ __ _ __ _   _               \e[0;90m|\e[0;31mXXXXXXXXXXX\e[0;90m|\e[0;35m XX    X\e[0;90m|"
-echo -e "\e[0;34;1m| |_ | | | | '__| '__| | | |              \e[0;90m|\e[0;31mX   XXX   X\e[0;90m|\e[0;35m XX    X\e[0;90m|"
-echo -e "\e[0;34;1m|  _|| |_| | |  | |  | |_| |              \e[0;90m|\e[0;31mX   XXX   X\e[0;90m|\e[0;35mXXXXXXXX\e[0;90m|"
-echo -e "\e[0;34;1m|_|  |___,_|_|  |_|  |___, |              \e[0;90m|\e[0;31mXXXXXXXXXXX\e[0;90m|\e[0;35m XX    X\e[0;90m|"
-echo -e "\e[0;34;1m ____        _ _     _|___/               \e[0;90m|\e[0;31mX   XXX   X\e[0;90m|\e[0;35m XX    X\e[0;90m|"
-echo -e "\e[0;34;1m| __ ) _   _(_) | __| (_)_ ___  __ _      \e[0;90m|\e[0;31mX   XXX   X\e[0;90m|\e[0;35mXXXXXXXX\e[0;90m|"
-echo -e "\e[0;34;1m|  _ || | | | | |/ _  | |  _  |/ _  |     \e[0;90m|\e[0;31mXXXXXXXXXXX\e[0;90m|\e[0;35mXXXXXXXX\e[0;90m|"
-echo -e "\e[0;34;1m| |_) | |_| | | | (_| | | | | | (_| |     \e[0;90m|\e[0;31mX\e[0;90m|   |   |\e[0;31mX\e[0;90m|  |  |\e[0;35mXX\e[0;90m|"
-echo -e "\e[0;34;1m|____/|___,_|_|_||__,_|_|_| |_||__, |     \e[0;90m|\e[0;31mX\e[0;90m|   |   |\e[0;31mX\e[0;90m|  |  |\e[0;35mXX\e[0;90m|"
-echo -e "\e[0;34;1m                               |___/      \e[0;90m|\e[0;31mX\e[0;90m|   |   |\e[0;31mX\e[0;90m|  |  |\e[0;35mXX\e[0;90m|"
-tput setaf 0
-
+echo -e "                                                                 \e[0;90m____________"
+echo -e "                                                                \e[0;90m|\e[0;35mXXXXXXXXXXXX\e[0;90m|"
+echo -e "                                                                \e[0;90m|\e[0;35mX    XX    x\e[0;90m|"
+echo -e "                                                                \e[0;90m|\e[0;35mX    XX    x\e[0;90m|"
+echo -e "                                                         \e[0;90m_______\e[0;90m|\e[0;35mX\e[0;90m__\e[0;35mXXXXXXXXX\e[0;90m|"
+echo -e "                                                        \e[0;90m|\e[0;31mXXXXXXXXXXX\e[0;90m| \e[0;35mXX    X\e[0;90m|"
+echo -e "                                                        \e[0;90m|\e[0;31mX   XXX   X\e[0;90m| \e[0;35mXX    X\e[0;90m|"
+echo -e "              \e[0;34;1m _____                                    \e[0;90m|\e[0;31mX   XXX   X\e[0;90m|\e[0;35mXXXXXXXX\e[0;90m|"
+echo -e "              \e[0;34;1m|  ___|   _ _ __ _ __ _   _               \e[0;90m|\e[0;31mXXXXXXXXXXX\e[0;90m|\e[0;35m XX    X\e[0;90m|"
+echo -e "              \e[0;34;1m| |_ | | | | '__| '__| | | |              \e[0;90m|\e[0;31mX   XXX   X\e[0;90m|\e[0;35m XX    X\e[0;90m|"
+echo -e "              \e[0;34;1m|  _|| |_| | |  | |  | |_| |              \e[0;90m|\e[0;31mX   XXX   X\e[0;90m|\e[0;35mXXXXXXXX\e[0;90m|"
+echo -e "              \e[0;34;1m|_|  |___,_|_|  |_|  |___, |              \e[0;90m|\e[0;31mXXXXXXXXXXX\e[0;90m|\e[0;35m XX    X\e[0;90m|"
+echo -e "              \e[0;34;1m ____        _ _     _|___/               \e[0;90m|\e[0;31mX   XXX   X\e[0;90m|\e[0;35m XX    X\e[0;90m|"
+echo -e "              \e[0;34;1m| __ ) _   _(_) | __| (_)_ ___  __ _      \e[0;90m|\e[0;31mX   XXX   X\e[0;90m|\e[0;35mXXXXXXXX\e[0;90m|"
+echo -e "              \e[0;34;1m|  _ || | | | | |/ _  | |  _  |/ _  |     \e[0;90m|\e[0;31mXXXXXXXXXXX\e[0;90m|\e[0;35mXXXXXXXX\e[0;90m|"
+echo -e "              \e[0;34;1m| |_) | |_| | | | (_| | | | | | (_| |     \e[0;90m|\e[0;31mX\e[0;90m|   |   |\e[0;31mX\e[0;90m|  |  |\e[0;35mXX\e[0;90m|"
+echo -e "              \e[0;34;1m|____/|___,_|_|_||__,_|_|_| |_||__, |     \e[0;90m|\e[0;31mX\e[0;90m|   |   |\e[0;31mX\e[0;90m|  |  |\e[0;35mXX\e[0;90m|"
+echo -e "              \e[0;34;1m                               |___/      \e[0;90m|\e[0;31mX\e[0;90m|   |   |\e[0;31mX\e[0;90m|  |  |\e[0;35mXX\e[0;90m|"
 tput setaf 6
-echo "                   Ingrese Usuario: "
+tput cup 20 
+echo "Nº Apartamento: "
+tput cup 20 16
 read usuario
-echo "                   Pin:  "
-read pin
+tput cup 21
+echo -e "Pin: "
+tput cup 21 5
+read -s pin
+pinencrypt=`echo -n $pin | base64`
 tput setaf 4
-echo $usuario>../BBDD/DatosTemporales/temp1.txt
-# AGUSTIN, VOY A GUARDAR EL NOMBRE DEL USUARIO EN UNA ARCHIVO TEM1 PARA PODER TRABAJAR EN EL 
-# REGISTRO, CUANDO EL LOGIN ESTE FUNCIONANDO VAS A TENER QUE HACER LO MISMO, PERO CON USUARIO #VERIFICADO 
-clear
-# CONFIGURAR LOGIN AGUSTIN 
-sh menuUsuario.sh
-done
+if test `grep ":$usuario:$pinencrypt:" ../BBDD/Tablas/datosLogin.txt | cut -d: -f1,2`
+then	
+	echo $usuario>../BBDD/DatosTemporales/temp1.txt
+	clear
+	tput sgr 0
+	sh menuUsuario.sh
+else
+	tput setab 1
+	tput setaf 7
+	tput cup 23
+	echo -e "Apartamento o contraseña incorrectos"
+	read -s pause
+fi
+tput sgr 0
+sh ../Menus/LOGIN.sh
--- Menus/menuUsuario.sh
+++ Menus/menuUsuario.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 clear
+tput setab 7
+tput setaf 4
 seleccion=0
 while test $seleccion -ne 5
 do
   clear
-  tput setab 9
+  tput setab 7
   tput setaf 4
   echo "1. Reservar"
   echo "2. Denunciar"
@@ -13,29 +15,41 @@ do
   echo "5. Cerrar sesion"
   read seleccion
   case $seleccion in
-    1) sh ../usuario/reserva.sh
+    1)
+      tput sgr 0 
+      sh ../usuario/reserva.sh
     ;;
-    2) sh ../usuario/denuncias.sh
+    2)
+      tput sgr 0 
+      sh ../usuario/denuncias.sh
     ;;
-    3) sh ../usuario/cambiarpass.sh
+    3)
+      tput sgr 0
+      sh ../usuario/cambiarpass.sh
     ;;
-    4) sh ../usuario/iniciarevento.sh
+    4)
+      tput sgr 0 
+      sh ../usuario/iniciarevento.sh
     ;;
     5)
-    tput setab 2
-    tput setaf 0
-    echo "Ha cerrado la sesion"
-    tput setaf 4
-    tput setab 9
-    clear
+      tput setab 2
+      tput setaf 0
+      clear
+      echo "Ha cerrado la sesion"
+      read -s pause
+      tput sgr 0
+      sh ../Menus/LOGIN.sh 
+      clear
     ;;
     *) 
-    tput setab 1
-    tput setaf 0
-    echo "Ingrese una opcion valida"
-    tput setaf 4
-    tput setab 9
-    clear
+      tput setab 1
+      tput setaf 0
+      clear
+      echo "Ingrese una opcion valida"
+      read -s pause
+      sh ../Menus/menuUsuario.sh
+      tput sgr 0
+      clear
     ;;
   esac
 done
