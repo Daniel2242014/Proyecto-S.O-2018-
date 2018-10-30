3ad27fd6f7a546d6a1529a14975d6198a4acddbc
Agustin Perez <NZXT@192.168.3.107>
2018-10-30 07:08:24 +0100

    aaaagria

--- Menus/menuAdministrador.sh
+++ Menus/menuAdministrador.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-seleccion=0
-while test $seleccion -ne 4
+seleccion=1
+while test $seleccion -ne 0
 do
 tput sgr 0
 tput setab 7
@@ -9,7 +9,10 @@ clear
 echo -e "\e[0;47;34m1. \e[0;47;30mVer denuncias"
 echo -e "\e[0;47;34m2. \e[0;47;30mActualizar pagos"
 echo -e "\e[0;47;34m3. \e[0;47;30mEditar usuarios"
-echo -e "\e[0;47;34m4. \e[0;47;30mCerrar sesion"
+echo -e "\e[0;47;34m4. \e[0;47;30mHabilitar apartamento"
+echo -e "\e[0;47;34m5. \e[0;47;30mDeshabilitar apartamento"
+echo -e "\e[0;47;34m6. \e[0;47;30mListar apartamentos habilitados"
+echo -e "\e[0;47;34m0. \e[0;47;30mCerrar sesion"
 read seleccion
 case $seleccion in
 	1) 
@@ -28,6 +31,21 @@ case $seleccion in
 		sh ../admin/editarusers.sh
 	;;
 	4)
+		tput sgr 0
+		tput cnorm	
+		sh ../admin/altaDpto.sh
+	;;
+	5)
+		tput sgr 0
+		tput cnorm	
+		sh ../admin/bajaDpto.sh
+	;;
+	6)
+		tput sgr 0
+		tput cnorm	
+		sh ../admin/listarDptos.sh
+	;;	
+	0)
 		rm ../BBDD/DatosTemporales/temp1.txt
 		touch ../BBDD/DatosTemporales/temp1.txt
 		tput setab 2
@@ -48,6 +66,7 @@ case $seleccion in
 		read -s pause
 		tput cnorm
 		tput sgr 0
+		seleccion=9
 		clear
 	;;
 esac
